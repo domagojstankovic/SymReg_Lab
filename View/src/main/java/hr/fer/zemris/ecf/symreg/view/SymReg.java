84e99f70ff727a8cfa142981553252104368b58c
dstank25 <domagoj.stankovic@gmail.com>
2015-06-06 15:39:37 +0200

    Initial commit

---
+++ View/src/main/java/hr/fer/zemris/ecf/symreg/view/SymReg.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.ecf.symreg.view;
+
+import javax.swing.*;
+
+/**
+ * Created by Domagoj on 06/06/15.
+ */
+public class SymReg extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
